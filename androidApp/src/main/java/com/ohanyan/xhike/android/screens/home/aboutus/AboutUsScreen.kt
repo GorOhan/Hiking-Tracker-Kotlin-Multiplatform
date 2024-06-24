62fe74e689d4114559a3035ca7e1049eeb7c53c9
gorohanyan <gorwork21@gmail.com>
2024-06-24 12:49:17 +0400

    add about us text.

--- androidApp/src/main/java/com/ohanyan/xhike/android/screens/home/aboutus/AboutUsScreen.kt
+++ androidApp/src/main/java/com/ohanyan/xhike/android/screens/home/aboutus/AboutUsScreen.kt
@@ -1,7 +1,6 @@
 package com.ohanyan.xhike.android.screens.home.aboutus
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
@@ -9,7 +8,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.navigation.NavController
+import com.ohanyan.xhike.android.R
 import org.koin.androidx.compose.getViewModel
 
 @Composable
@@ -20,17 +22,15 @@ fun AboutUsScreen(
 
     Box(
         modifier = Modifier
-            .background(color = MaterialTheme.colorScheme.background)
-            .fillMaxSize(),
+            .fillMaxSize()
+            .background(color = MaterialTheme.colorScheme.background),
         contentAlignment = Alignment.Center,
     ) {
         Text(
-            modifier = Modifier.clickable {
-              //  navController.navigate(Screen.MapScreen.route)
-            },
-            text = "About Us",
+            text = stringResource(id = R.string.about_us_body),
             color = MaterialTheme.colorScheme.tertiary,
-            style = MaterialTheme.typography.titleLarge
+            style = MaterialTheme.typography.titleMedium,
+            textAlign = TextAlign.Center
         )
     }
 }
